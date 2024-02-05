79a7d8b74b931212f6b4a52478428a72ba20cd3b
Julius Lansner <axiiss@live.dk>
2024-02-05 23:16:18 +0100

    Update main.py

--- Trading/main.py
+++ Trading/main.py
@@ -9,10 +9,17 @@ if response.status_code == 200:
     data = response.json()
     orders = data['payload']['orders']
     sell_orders = []
-    
+    sell_orders_string = set()
     for order in orders:
+        
         if order['order_type'] == 'sell' and order['platform'] =='pc' and order['user']['status'] == 'ingame':
-            sell_orders.append({'user': order['user']['ingame_name'], 'price': order['platinum']})
-           #print(f"User: {order['user']['ingame_name']}, Price: {order['platinum']}")
+            order_string = f"{order['user']['ingame_name']}-{order['platinum']}"
+            if order_string not in sell_orders_string:
+                sell_orders.append({'user': order['user']['ingame_name'], 'price': float(order['platinum'])})
+                sell_orders_string.add(order_string)
+    sorted_sell_orders = sorted(sell_orders, key=lambda k: k['price'])
+        
+    for order in sorted_sell_orders:
+        print(f"User: {order['user']}, Price: {order['price']}")
 else:
     print(f"failed, status code: {response.status_code}")
