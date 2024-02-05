73c0d2ed58ca48e41636cf3c6f8ed57ea68fdbc8
Julius Lansner <axiiss@live.dk>
2024-02-05 22:57:02 +0100

    Update main.py

--- Trading/main.py
+++ Trading/main.py
@@ -4,11 +4,15 @@ platform = 'pc'
 response = requests.get(f'https://api.warframe.market/v1/items/{item}/orders?include=item')
 
 #check to see if the request was successful
+
 if response.status_code == 200:
     data = response.json()
     orders = data['payload']['orders']
+    sell_orders = []
+    
     for order in orders:
         if order['order_type'] == 'sell' and order['platform'] =='pc' and order['user']['status'] == 'ingame':
-            print(f"User: {order['user']['ingame_name']}, Price: {order['platinum']}")
+            sell_orders.append({'user': order['user']['ingame_name'], 'price': order['platinum']})
+           #print(f"User: {order['user']['ingame_name']}, Price: {order['platinum']}")
 else:
     print(f"failed, status code: {response.status_code}")
