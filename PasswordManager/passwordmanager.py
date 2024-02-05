12367641dda13020f717acc2fd61ef92a5dfb986
Julius Lansner <axiiss@live.dk>
2024-02-05 18:14:46 +0100

    Initial commit

---
+++ PasswordManager/passwordmanager.py
@@ -0,0 +1,21 @@
+pwd = input("what is the master password?")
+
+def view():
+    pass
+
+def add():
+    pass
+
+
+while True:
+    mode = input("add new or view existing passwords? (add, view or q to quit)")
+    if mode == "q":
+        break
+    
+    
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else: 
+        print("invalid mode")
